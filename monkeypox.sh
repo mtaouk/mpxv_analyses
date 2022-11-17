a6447a3bd03d5da07ac923d55a860ddce5abf319
mtaouk <90819350+mtaouk@users.noreply.github.com>
2022-11-17 13:49:53 +1100

    alignments

--- monkeypox.sh
+++ monkeypox.sh
@@ -53,13 +53,6 @@ iqtree -s consensus_pass_mafft_nogaps_masked_nodupref_0.1N.aln -B 1000 -nt 40 --
 snp-dists consensus_pass_mafft_nogaps_masked_nodupref_0.1N.aln > snp_dist.tsv
 
 
-######### Making alignments that are just same patients
-# Extract from processed alignment
-for i in {1..68}; do seqkit grep -r -p "^${i}_" -p ON563414.3.masked /home/taouk/Monkeypox/alignments/consensus_pass_mafft_nogaps_masked_nodupref_0.1N.aln > ${i}.aln; done
-ls *.aln > input.txt
-for i in $(cat input.txt); do snipit ${i} -o ${i} -r ON563414.3.masked -f pdf; done
-
-
 ######### Nextclade Local
 cd Monkeypox/nextclade
 seqkit grep -vrp "ON563414.3" /home/taouk/Monkeypox/alignments/consensus_pass.fasta > local.fasta
@@ -69,8 +62,6 @@ nextclade dataset list
 #|hMPXV                              │ NC_063383.1 (*)                   │ 2022-09-27T12:00:00Z (*) │ name=hMPXV                    │ Add lineages A.2.2 and B.1.10-12  │
 #│ 'Human Monkeypox (hMPXV)'         │ 'MPXV-M5312_HM12_Rivers'          │                          │ reference=NC_063383.1 (*)     │                                   │
 #│                                   │                                   │                          │ tag=2022-09-27T12:00:00Z (*)  │                      
-
-nextclade run -D hmpxv/ local.fasta --output-all local
 
 
 ######### Nextclade Global
@@ -83,9 +74,9 @@ seqtk comp sequences_global_local.fasta > comp.tsv
 conda activate nextclade
 nextclade run -D hmpxv/ sequences_global_local.fasta --output-all global
 
+nextclade run -D hmpxv/ /home/taouk/Monkeypox/nextclade/sequences_global_local.fasta --output-all global_hmpxv_141122
 
-######### Global sequences making alignment from scratch
-
+######### Global Trees
 cp /home/taouk/Monkeypox/nextclade/global/nextclade.aligned.fasta /home/taouk/Monkeypox/global/nextclade.aligned.fasta
 cd /home/taouk/Monkeypox/global
 conda activate Base
@@ -113,4 +104,26 @@ for i in $(cat ../alignments/ont_85_pass.txt); do sed 's/^##.*//p' ${i}.vcf > ${
 rm *.vcf
 for i in $(cat ../alignments/twist_90_pass.txt); do cat /home/taouk/Monkeypox/twist/twist_consensus/75/${i}.variants.tsv > ${i}.tsv; done
 mutation-profile -f ../mpx_us_22.fasta -m combine -r "ON563414.3" > output.txt
+
+
+
+
+######### ALignments per patient using Rivers reference and masking for repeats (attempt)
+conda activate nextclade
+cd /home/taouk/Monkeypox/patient_alignment/rivers_usa
+nextclade run -D /home/taouk/Monkeypox/nextclade/hmpxv sequences.fasta --output-all local
+# seperate
+conda activate Base
+for i in {1..68}; do seqkit grep -r -p "^${i}_" -p ON563414.3.masked  /home/taouk/Monkeypox/patient_alignment/rivers_usa/nextclade.aligned.fasta > individuals/${i}.aln; done
+cd individuals
+ls *.aln > input.txt
+for i in $(cat input.txt); do snipit ${i} -o ${i} -r ON563414.3.masked -f pdf --height 3 --width 10; done
+
+cd usa_coords
+for i in {1..68}; do seqkit grep -r -p "^${i}_" -p ON563414.3.masked consensus_pass_augur.aln > individuals/${i}.aln; done
+cd individuals
+ls *.aln > input.txt
+for i in $(cat input.txt); do snipit ${i} -o ${i} -r ON563414.3.masked -f pdf --height 3 --width 10; done
+
+
 
